5b1018dab3121e891b8886628154f7cc93ba024c
dalilalavras <dalilalavras@hotmail.com>
2022-06-28 14:43:51 -0300

    Primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Redução da barra de navegação
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Encolher a barra de navegação 
+    navbarShrink();
+
+    // Encolher a barra de navegação quando dá scroll
+    document.addEventListener('scroll', navbarShrink);
+
+    // Ativação do bootstrap na navegação
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Recolher a barra de navegação quando o altenador estiver aparecendo
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
